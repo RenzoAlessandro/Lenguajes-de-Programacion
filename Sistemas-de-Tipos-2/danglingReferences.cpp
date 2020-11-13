324e6c3c3e46e5e1cc46c8a955e74f6c33bbc44d
RenzoAlessandro <renzo.sucari@ucsp.edu.pe>
2020-11-12 21:58:56 -0500

    Dangling References

---
+++ Sistemas-de-Tipos-2/danglingReferences.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main(){
+    //Ejemplo Nº1
+    int * puntero_x = new int;
+    delete puntero_x;
+    int i = *puntero_x; // error, puntero_x ha sido eliminado!
+
+    //Ejemplo Nº2
+    int * arrayPtr1;
+    int * arrayPtr2 = new int[100];
+    arrayPtr1 = arrayPtr2;
+    delete [] arrayPtr2;
+    //Ahora, arrayPtr1 está colgando, porque el almacenamiento 
+    //dinámico al que apuntaba ha sido desasignado.
+}
